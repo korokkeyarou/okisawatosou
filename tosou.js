436fee9ea283bbb518a1458ef929ec3ac78cc1d7
korokke <128043117+korokkeyarou@users.noreply.github.com>
2023-04-14 12:12:07 +0900

    Update tosou.js

--- tosou.js
+++ tosou.js
@@ -4,6 +4,8 @@ $(function(){
         var position=$(id).offset().top;
         $('html,body').animate({
             'scrollTop':position
+
+
         })
     })
     $('.title').click(function(){
@@ -11,8 +13,40 @@ $(function(){
         $('html, body').animate({
           'scrollTop':0
         },500);
+        
       });
     $('.taitoru').fadeIn();
+    $('.work').click(function(){
+      $(this).find('.foto').show();
+    })
+    
+    $('.next-btn').click(function(){
+      var $displaySlide =$('.active');
+      $('.active').removeClass('active');
+      $displaySlide.next().addClass('active');
+    })
+    $('.pre-btn').click(function(){
+      var $displaySlide =$('.active');
+      $('.active').removeClass('active');
+      $displaySlide.prev().addClass('active');
+    })
+    
+    
+    $('.foot').click(function(){
+      var slideIndex = $('.img').index($('.active'));
+     
+      $('.foot').show();
+      if (slideIndex===0){
+        $('.pre-btn').hide();
+      }else if(slideIndex===2){
+        $('.next-btn').hide();
+      }
+    })
+    $('.fa-times').click(function(){
+      $('.foto').fadeOut();
+      
+    })
+   
     
       
 });
